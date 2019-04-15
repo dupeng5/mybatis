80592f5393a64bae23d2debf9c97f1e9ac35ad11
dupeng <dupeng5@asiainfo.com>
2019-04-15 17:16:20 +0800

    jdk8新特性目录

---
+++ src/main/java/com/example/mybatis/jdk8newFunction/defaultfunction/MainTest.java
@@ -0,0 +1,12 @@
+package com.example.mybatis.jdk8newFunction.defaultfunction;
+
+/**
+ * @Author: dupeng5
+ * @Date: 2019/4/15 10:44
+ * @Version 1.0
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        StaticInterface.printlnMessage();
+    }
+}
