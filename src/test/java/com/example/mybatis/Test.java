ec7a8c07abe2db2215716e79ec6e36be91ff47f7
dupeng <dupeng5@asiainfo.com>
2019-04-18 18:09:20 +0800

    整合日志

--- src/test/java/com/example/mybatis/Test.java
+++ src/test/java/com/example/mybatis/Test.java
@@ -8,6 +8,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static java.lang.Thread.sleep;
+
 /**
  * @Author: dupeng5
  * @Date: 2019/4/17 18:13
@@ -25,7 +27,18 @@ public class Test {
 
     @org.junit.Test
     public void test(){
-        stringRedisTemplate.opsForValue().set("code","123456");
-        System.out.println(stringRedisTemplate.opsForValue().get("code"));
+
+        try {
+            while(true){
+                stringRedisTemplate.opsForValue().append("code","数据"+String.valueOf(System.currentTimeMillis()));
+                stringRedisTemplate.opsForValue().set(String.valueOf(System.currentTimeMillis()),String.valueOf(System.currentTimeMillis()));
+                Thread.sleep(100);
+            }
+        } catch (Exception e){
+
+        }finally {
+
+        }
+//        System.out.println(stringRedisTemplate.opsForValue().get("code"));
     }
 }
